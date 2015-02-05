62cbb69ff3a35ec90813525139e12f34b0e76527
matteomanzo <imatteo.manzo@gmail.com>
2015-02-05 18:07:28 +0000

    interface connected, app working!

--- public/interface.js
+++ public/interface.js
@@ -1,6 +1,13 @@
-$(document).ready(function() {
-  $.get('https://api.github.com/users/bebbs', function(user) {
+function addProfileFromUsername(username) {
+  $.get('https://api.github.com/users/' + username, function(user) {
     var newProfile = Mustache.render($('.profile-container').html(), user);
     $('.github-container').append(newProfile);
+  });
+}
+
+$(document).ready(function() {
+  $('#add-profile').on('submit', function(event) {
+    event.preventDefault();
+    addProfileFromUsername($('.search-box').val());
   });
 });
